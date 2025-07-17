b8fd2dea45e362dd69152ae1884e48d35b862768
Gustavo Kamitani <gustavodocarmokamitani@gmail.com>
2025-07-17 16:31:51 -0300

    fix: api.ts - add / in baseURL

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://insumo-backend.onrender.com",
+  baseURL: "https://insumo-backend.onrender.com/",
 });
 
 export default api;
